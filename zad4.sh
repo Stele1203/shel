6548ac079a02ee98977c075bb02fbd6affc49558
Stefan <52536505+Stele1203@users.noreply.github.com>
2023-12-03 19:07:14 +0100

    Update zad4.sh

--- zad4.sh
+++ zad4.sh
@@ -1,37 +1,41 @@
-# ./zad4.sh imefolder
+# ./zad4.sh folder
 
 if [ $# -ne 1 ]
 then
-    echo program treba da ima samo jedan argument
+    echo program treba dai ma jedan argument
     exit 1
 fi
 
+
 if ! [ -d $1 ]
 then
-    echo argument koji ste zadali nije folder
+    echo nije folder
     exit 2
 fi
 
 folder=$1
 
-for a in $folder/* 
+for a in $folder/*
 do
-
     aBase=$(basename $a)
     aExt=${aBase#*.}
-    
-
-    for b in $folder/*
-    do
-        bBase=$(basename $b)
-        bExt=${bBase#*.}
-        if ! [ "$bExt" == "$aExt" ]
-        then
-           echo Ekstenzije se razlikuju
-            exit 3
-        fi
-
-    done
-    
+    if ! [ -d $a ] 
+    then
+        for b in $folder/*
+        do
+            if ! [ -d $b ]
+            then
+            
+                bBase=$(basename $b)
+                bExt=${bBase#*.}
+                if ! [ "$bExt" == "$aExt" ]
+                then
+                    echo ekstenzije nisu iste
+                    exit 3
+                fi
+            fi
+        done
+    fi
 done
-echo Ekstenzije su iste
+
+echo ekstenzije su iste
